2b694b59fcf7608db9af8cb306fce367bb1c8f21
rishi14052003 <143413225+rishi14052003@users.noreply.github.com>
2025-09-17 11:36:44 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,74 +1,45 @@
-import react, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-const drumPads=[
-  {
-    key:"Q",
-    id:"Heater 1",
-    url:"https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
-  },
-  {
-    key:"W",
-    id:"Heater 2",
-    url:"https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
-  },
-  {
-    key:"E",
-    id:"Heater 3",
-    url:"https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
-  },
-  {
-    key:"A",
-    id:"Heater 4",
-    url:"https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
-  },
-  {
-    key:"S",
-    id:"Clap",
-    url:"https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
-  },
-  {
-    key:"D",
-    id:"Open-HH",
-    url:"https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
-  },
-  {
-    key:"Z",
-    id:"Kick-n'-Hat",
-    url:"https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
-  },
-  {
-    key:"X",
-    id:"Kick",
-    url:"https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
-  },
-  {
-    key:"C",
-    id:"Closed-HH",
-    url:"https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3", 
-  }
-]
+
+const drumPads = [
+  { key: "Q", id: "Heater 1", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" },
+  { key: "W", id: "Heater 2", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" },
+  { key: "E", id: "Heater 3", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" },
+  { key: "A", id: "Heater 4", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" },
+  { key: "S", id: "Clap", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" },
+  { key: "D", id: "Open-HH", url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" },
+  { key: "Z", id: "Kick-n'-Hat", url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" },
+  { key: "X", id: "Kick", url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" },
+  { key: "C", id: "Closed-HH", url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" }
+];
 
 const App = () => {
   const [display, setDisplay] = useState("");
+
   const playSound = (key) => {
     const audio = document.getElementById(key);
     if (audio) {
-      audio.currentTime = 0;
-      audio.play();
+      audio.currentTime = 0; // restart sound from beginning
+      audio.play().catch((error) => {
+        console.log("Playback prevented:", error);
+      });
       const pad = drumPads.find((pad) => pad.key === key);
-      setDisplay(pad.id);
+      if (pad) setDisplay(pad.id);
     }
   };
+
   const handleKeyPress = (e) => {
     const key = e.key.toUpperCase();
-    if (drumPads.find((pad) => pad.key === key)) {
+    if (drumPads.some((pad) => pad.key === key)) {
       playSound(key);
     }
   };
+
   useEffect(() => {
     document.addEventListener("keydown", handleKeyPress);
     return () => document.removeEventListener("keydown", handleKeyPress);
   }, []);
+
   return (
     <div id="drum-machine">
       <div className="pads">
@@ -88,4 +59,5 @@ const App = () => {
     </div>
   );
 };
+
 export default App;
